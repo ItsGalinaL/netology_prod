22e022f701422cf66bb351cbd95a5e50a9bdaf04
ItsgalinaL <S.galina.l@yandex.ru>
2023-05-01 16:08:28 +0700

    Исправление замечаний

--- src/test/java/ru/netology/prod/ProductManagerTest.java
+++ src/test/java/ru/netology/prod/ProductManagerTest.java
@@ -2,12 +2,13 @@ package ru.netology.prod;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ProductManagerTest {
 
     @Test
-    void testSearch() {
+    void testSearchEmptyResult() {
+        Product[] expected = new Product[0];
         ProductRepository repository = new ProductRepository();
         ProductManager manager = new ProductManager(repository);
         Product book1 = new Book(1, "Золотой Козлёнок", "Ильф и Петров");
@@ -17,6 +18,51 @@ class ProductManagerTest {
         Product smart = new Smartphone(3, "Samsung 1");
         manager.addProduct(smart);
 
-        assertEquals(2, manager.searchBy("Козлёнок").length);
+        Product[] actual = manager.searchBy("молоко");
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void testSearchOneResult() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager(repository);
+        Product[] expected = new Product[1];
+
+        Product book1 = new Book(1, "Золотой Козлёнок", "Ильф и Петров");
+        manager.addProduct(book1);
+
+        Product book2 = new Book(2, "Козлёнок, который любил считать", "А. Азимов");
+        manager.addProduct(book2);
+
+        Product smart = new Smartphone(3, "Samsung 1");
+        manager.addProduct(smart);
+        expected[0] = smart;
+
+        Product[] actual = manager.searchBy("1");
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void testSearchManyResults() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager(repository);
+        Product[] expected = new Product[2];
+
+        Product book1 = new Book(1, "Золотой Козлёнок", "Ильф и Петров");
+        manager.addProduct(book1);
+        expected[0] = book1;
+
+        Product book2 = new Book(2, "Козлёнок, который любил считать", "А. Азимов");
+        manager.addProduct(book2);
+        expected[1] = book2;
+
+        Product smart = new Smartphone(3, "Samsung 1");
+        manager.addProduct(smart);
+
+        Product[] actual = manager.searchBy("Козлёнок");
+
+        assertArrayEquals(expected, actual);
     }
 }
--- src/test/java/ru/netology/prod/ProductRepositoryTest.java
+++ src/test/java/ru/netology/prod/ProductRepositoryTest.java
@@ -8,35 +8,53 @@ class ProductRepositoryTest {
 
     @Test
     void testEmptyRepository() {
+        Product[] expected = new Product[0];
+
         ProductRepository repository = new ProductRepository();
 
-        assertEquals(0, repository.findAll().length);
+        Product[] actual = repository.findAll();
+
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void testRepositoryFindAll() {
+        Product[] expected = new Product[2];
         ProductRepository repository = new ProductRepository();
+
         Product book = new Book(1, "Золотой телёнок", "Ильф и Петров");
         repository.addProduct(book);
+        expected[0] = book;
 
         Product book2 = new Book(2, "Я робот", "А. Азимов");
         repository.addProduct(book2);
+        expected[1] = book2;
 
-        assertEquals(2, repository.findAll().length);
+        Product[] actual = repository.findAll();
+
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void testRepositoryRemove() {
+        Product[] expected = new Product[2];
+
         ProductRepository repository = new ProductRepository();
+
         Product book1 = new Book(1, "Золотой телёнок", "Ильф и Петров");
         repository.addProduct(book1);
+        expected[0] = book1;
+
         Product book2 = new Book(2, "Я робот", "А. Азимов");
         repository.addProduct(book2);
+
         Product smart = new Smartphone(3, "Samsung 1");
         repository.addProduct(smart);
+        expected[1] = smart;
 
         repository.removeProduct(2);
+        Product[] actual = repository.findAll();
 
-        assertEquals(2, repository.findAll().length);
+        assertArrayEquals(expected, actual);
     }
 }
